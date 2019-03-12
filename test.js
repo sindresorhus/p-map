f00cbd79bbe5a3eef6f0290189550fb8512e1dea
Dimitri Benin <dimitri.benin@gmail.com>
2019-03-12 23:51:55 +0100

    Update dev dependencies

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ import delay from 'delay';
 import inRange from 'in-range';
 import timeSpan from 'time-span';
 import randomInt from 'random-int';
-import m from '.';
+import pMap from '.';
 
 const input = [
 	Promise.resolve([10, 300]),
@@ -11,17 +11,17 @@ const input = [
 	[30, 100]
 ];
 
-const mapper = ([val, ms]) => delay(ms).then(() => val);
+const mapper = ([value, ms]) => delay(ms, {value});
 
 test('main', async t => {
 	const end = timeSpan();
-	t.deepEqual(await m(input, mapper), [10, 20, 30]);
+	t.deepEqual(await pMap(input, mapper), [10, 20, 30]);
 	t.true(inRange(end(), 290, 430));
 });
 
 test('concurrency: 1', async t => {
 	const end = timeSpan();
-	t.deepEqual(await m(input, mapper, {concurrency: 1}), [10, 20, 30]);
+	t.deepEqual(await pMap(input, mapper, {concurrency: 1}), [10, 20, 30]);
 	t.true(inRange(end(), 590, 760));
 });
 
@@ -29,7 +29,7 @@ test('concurrency: 4', async t => {
 	const concurrency = 4;
 	let running = 0;
 
-	await m(new Array(100).fill(0), async () => {
+	await pMap(new Array(100).fill(0), async () => {
 		running++;
 		t.true(running <= concurrency);
 		await delay(randomInt(30, 200));
@@ -38,34 +38,34 @@ test('concurrency: 4', async t => {
 });
 
 test('handles empty iterable', async t => {
-	t.deepEqual(await m([], mapper), []);
+	t.deepEqual(await pMap([], mapper), []);
 });
 
 test('async with concurrency: 2 (random time sequence)', async t => {
 	const input = new Array(10).map(() => randomInt(0, 100));
-	const mapper = value => delay(value).then(() => value);
-	const result = await m(input, mapper, {concurrency: 2});
+	const mapper = value => delay(value, {value});
+	const result = await pMap(input, mapper, {concurrency: 2});
 	t.deepEqual(result, input);
 });
 
 test('async with concurrency: 2 (problematic time sequence)', async t => {
 	const input = [100, 200, 10, 36, 13, 45];
-	const mapper = value => delay(value).then(() => value);
-	const result = await m(input, mapper, {concurrency: 2});
+	const mapper = value => delay(value, {value});
+	const result = await pMap(input, mapper, {concurrency: 2});
 	t.deepEqual(result, input);
 });
 
 test('async with concurrency: 2 (out of order time sequence)', async t => {
 	const input = [200, 100, 50];
-	const mapper = value => delay(value).then(() => value);
-	const result = await m(input, mapper, {concurrency: 2});
+	const mapper = value => delay(value, {value});
+	const result = await pMap(input, mapper, {concurrency: 2});
 	t.deepEqual(result, input);
 });
 
 test('enforce number in options.concurrency', async t => {
-	await t.throws(m([], () => {}, {concurrency: 0}), TypeError);
-	await t.throws(m([], () => {}, {concurrency: undefined}), TypeError);
-	await t.notThrows(m([], () => {}, {concurrency: 1}));
-	await t.notThrows(m([], () => {}, {concurrency: 10}));
-	await t.notThrows(m([], () => {}, {concurrency: Infinity}));
+	await t.throwsAsync(pMap([], () => {}, {concurrency: 0}), TypeError);
+	await t.throwsAsync(pMap([], () => {}, {concurrency: undefined}), TypeError);
+	await t.notThrowsAsync(pMap([], () => {}, {concurrency: 1}));
+	await t.notThrowsAsync(pMap([], () => {}, {concurrency: 10}));
+	await t.notThrowsAsync(pMap([], () => {}, {concurrency: Infinity}));
 });
