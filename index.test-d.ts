9834e9dc20326554a318525133824f9cadeac751
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-04-06 15:26:20 +0000

    Refactor TypeScript definition to CommonJS compatible export (#18)

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,21 +1,16 @@
-import {expectType} from 'tsd-check';
-import pMap, {Options, Mapper} from '.';
+import {expectType} from 'tsd';
+import pMap = require('.');
+import {Options, Mapper} from '.';
 
-const sites = [
-	'ava.li',
-	'todomvc.com',
-	'github.com'
-];
+const sites = ['ava.li', 'todomvc.com', 'github.com'];
 
-const numbers = [
-	0,
-	1,
-	2
-];
+const numbers = [0, 1, 2];
 
 const asyncMapper = async (site: string) => site;
-const asyncSyncMapper = (site: string, index: number) => index > 1 ? site : Promise.resolve(site);
-const multiResultTypeMapper = (site: string, index: number) => index > 1 ? site.length : site;
+const asyncSyncMapper = (site: string, index: number) =>
+	index > 1 ? site : Promise.resolve(site);
+const multiResultTypeMapper = (site: string, index: number) =>
+	index > 1 ? site.length : site;
 
 expectType<Mapper>(asyncMapper);
 expectType<Mapper<string, string>>(asyncMapper);
@@ -27,13 +22,13 @@ expectType<Mapper<string, string | number>>(multiResultTypeMapper);
 expectType<Options>({});
 expectType<Options>({concurrency: 0});
 
-expectType<string[]>(await pMap(sites, asyncMapper));
-expectType<string[]>(await pMap(sites, asyncMapper, {concurrency: 2}));
+expectType<Promise<string[]>>(pMap(sites, asyncMapper));
+expectType<Promise<string[]>>(pMap(sites, asyncMapper, {concurrency: 2}));
 
-expectType<string[]>(await pMap(sites, asyncSyncMapper));
-expectType<(string | number)[]>(await pMap(sites, multiResultTypeMapper));
+expectType<Promise<string[]>>(pMap(sites, asyncSyncMapper));
+expectType<Promise<(string | number)[]>>(pMap(sites, multiResultTypeMapper));
 
-expectType<string[]>(await pMap(sites, (site: string) => site));
-expectType<number[]>(await pMap(sites, (site: string) => site.length));
+expectType<Promise<string[]>>(pMap(sites, (site: string) => site));
+expectType<Promise<number[]>>(pMap(sites, (site: string) => site.length));
 
-expectType<number[]>(await pMap(numbers, (number: number) => number * 2));
+expectType<Promise<number[]>>(pMap(numbers, (number: number) => number * 2));
